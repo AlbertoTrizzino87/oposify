55ab644c05e4becfa89da74be87f9cda09b94965
AlbertoTrizzino87 <alberto.trizzino01@gmail.com>
2018-05-28 02:25:04 +0200

    meta
    meta

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,8 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<meta name="csrf-token" content="{{ csrf_token() }}">
+    @yield('meta')
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Condensed:700" rel="stylesheet">
     <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
     <link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}" />
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.app')
 
+@section('meta')
+<meta name="description" content="Oposify es una plataforma que pone en contacto academias,preparadores y opositores y que mejorando el flujo de información">
+<meta name="keywords" content="preparar una oposición,oposición,academias,preparadores">
+<meta name="robots" content="index,follow">
+<meta property="og:title" content="Oposify" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="http://entregas.albertotrizzino.com" />
+<meta name="twitter:card" content="summary"></meta>
+<meta name=“twitter:url” content=“http://entregas.albertotrizzino.com”>
+<meta name=“twitter:title” content=“Oposify”>
+<meta name=“twitter:description” content="Oposify es una plataforma que pone en contacto academias,preparadores y opositores y que mejorando el flujo de información">
+
+
+@endsection
+
 @section('content')
 <main id="home">
     <header class="header">
@@ -62,7 +77,18 @@
 		<section id="about">
 			<span class="icon-cross" id="close-about"></span>
 			<h3>¿Que es tuoposify?</h3>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias ipsum pariatur quis accusantium! Eum doloremque, aperiam eius quisquam illum veniam delectus recusandae repellendus reprehenderit sequi. Doloribus repellendus corporis aut laborum placeat atque iure, veritatis illum consequatur voluptas quasi tempora nemo iste voluptatem, quos? Deleniti enim porro, cupiditate veritatis suscipit voluptatum necessitatibus id. Autem id reprehenderit laborum omnis libero error sint at eaque, est itaque officia adipisci provident similique facilis porro commodi ad voluptatum fuga impedit saepe. Doloremque obcaecati reiciendis repudiandae, ad nulla eveniet saepe autem totam, facilis veniam suscipit ea praesentium corporis consectetur odio aut qui incidunt quas vel quibusdam.</p>
+			<p>Oposify nace para llevar el estudio de las oposiciones al mundo digital. Hemos creado una plataforma que permite conectar academias, preparadores y opositores de todo el país para que el flujo de información sea lo más cómodo posible.</p>
+			<h5>Academias</h5>
+			<p>Si eres una academia tendrás a tu disposición una herramienta muy potente para gestionar todos tus alumnos y profesores, además de poder ponerte en contacto con ellos con un simple click.
+				En Oposify encontrarás la oportunidad de llegar a estudiantes de toda España y aumentar tu visibilidad.</p>
+			<h5>Preparadores</h5>
+			<p>Si eres un preparador a quien le gustan las nuevas tecnologías entonces esta es la plataforma que estabas buscando. Podrás subir los videos de tus clases y ponerlos a disposición de tus estudiantes. Por supuesto, tus alumnos también podrán acceder al temario, los apuntes e incluso los tests preparatorios. </p>
+			<h5>Opositores</h5>
+			<p>De la misma manera podrás disfrutar de clases grabadas en vídeo y de un temario siempre actualizado, un contacto directo con tu preparador y un espacio personal para organizar tu estudio.</p><br><br>
+			<p>Nuestro objetivo es transformar la enseñanza de las oposiciones y hacerla más transparente a través de juicios y comentarios que eleven el nivel de la oferta y que permitan a los demandantes elegir teniendo como referencia a otros usuarios que ya han pasado per el mismo camino. <br><br>
+			En Oposify sabemos de lo que hablamos porque lo hemos sufrido. Y hemos intercambiado ideas con muchos otros que también han estado allí. Y está claro: en muchas ocasiones quien decide empezar a estudiar una oposición no es consciente de todas las dificultades que pueden surgir durante el camino; y no tenía por qué saberlo. Hasta ahora. Por esa razón queremos crear una comunidad que permita ahorrarnos tiempo, dinero y mala decisiones. <br><br>
+			Si has decidido empezar con una oposición, búscala en nuestra plataforma, elige el mejor preparador y ya estás listo para empezar. A partir de entonces, todo dependerá de ti.</p>
+			<p>MUCHA SUERTE A TODOS!</p>
 		</section>
 
 		<section id="icons">
